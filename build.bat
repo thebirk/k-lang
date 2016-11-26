0a5f15df5ac89dbfcfbcaffc4e82a2917b04ba53
thebirk <pingnor@gmail.com>
2016-11-26 16:03:15 +0100

    Blah.

--- build.bat
+++ build.bat
@@ -5,6 +5,6 @@ set JNI_INCLUDE_WIN32=%JAVA_HOME%/include/win32
 set JNI_LIBRARY=%JAVA_HOME%/jre/lib/amd64/server
 
 
-cl /DHAS_JAVA_TREEVIEW /Feklc /I %JNI_INCLUDE% /I %JNI_INCLUDE_WIN32% main.c jvm.lib /link /LIBPATH:%JNI_LIBRARY_PATH%
+cl /DHAS_JAVA_TREEVIEW /Feklc /I %JNI_INCLUDE% /I %JNI_INCLUDE_WIN32% main.c jvm.lib /link /LIBPATH: %JNI_LIBRARY%
 
 endlocal
