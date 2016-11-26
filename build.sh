ff957bd8191765836c11b99d1cfc3ff9e60dfed5
thebirk <pingnor@gmail.com>
2016-11-26 15:08:17 +0100

    Added -Wreturntype to build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # K language compiler - Build script
-gcc -DHAS_JAVA_TREEVIEW -o klc -g main.c -ljvm -I/usr/lib/jvm/java-8-openjdk-amd64/include -I/usr/lib/jvm/java-8-openjdk-amd64/include/linux -L/usr/lib/jvm/java-8-openjdk-amd64/jre/lib/amd64/server
+gcc -Wreturn-type -DHAS_JAVA_TREEVIEW -o klc -g main.c -ljvm -I/usr/lib/jvm/java-8-openjdk-amd64/include -I/usr/lib/jvm/java-8-openjdk-amd64/include/linux -L/usr/lib/jvm/java-8-openjdk-amd64/jre/lib/amd64/server
 echo Build complete!
