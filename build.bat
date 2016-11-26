c60ce15420dcfb9b2173e11c55e12a1456e7037a
thebirk <pingnor@gmail.com>
2016-11-26 15:58:17 +0100

    Forgot set in build.bat.

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 @echo off
 setlocal
-JNI_INCLUDE=%JAVA_HOME%/include
-JNI_INCLUDE_WIN32=%JAVA_HOME%/include/win32
-JNI_LIBRARY=%JAVA_HOME%/jre/lib/amd64/server
+set JNI_INCLUDE=%JAVA_HOME%/include
+set JNI_INCLUDE_WIN32=%JAVA_HOME%/include/win32
+set JNI_LIBRARY=%JAVA_HOME%/jre/lib/amd64/server
 
 
 cl /DHAS_JAVA_TREEVIEW /Feklc /I %JNI_INCLUDE% /I JNI_INCLUDE_WIN32 main.c jvm.lib /link /LIBPATH:%JNI_LIBRARY_PATH%
