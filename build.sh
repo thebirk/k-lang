7db9544ffe5f7fb2fc4d74ae61fdcd79203c33e2
alek345 <pingnor@gmail.com>
2016-12-02 22:13:11 +0100

    Updated linux build script.

--- build.sh
+++ build.sh
@@ -7,10 +7,11 @@ JNI_LIBRARY=$JAVA_HOME/jre/lib/amd64/server
 LLVM_COMPS='core engine analysis interpreter native'
 LLVM_LDFLAGS=`llvm-config --ldflags`
 LLVM_CFLAGS=`llvm-config --cflags`
-LLVM_LIBS=`llvm-config --libs $LLVM_COMPS`
+LLVM_LIBS=`llvm-config --system-libs --libs $LLVM_COMPS`
+LLVM_LIBS_NOT_FOUND='-lffi'
 
 gcc -DHAS_JAVA_TREEVIEW -I$JNI_INCLUDE -I$JNI_INCLUDE_LINUX  $LLVM_CFLAGS -o klc.o -g -c src/main.c
-g++ -L$JNI_LIBRARY $LLVM_LDFLAGS -o klc klc.o $LLVM_LIBS -ljvm -ldl -lpthread -lffi -lncurses
+g++ -L$JNI_LIBRARY $LLVM_LDFLAGS -o klc klc.o -ljvm $LLVM_LIBS $LLVM_LIBS_NOT_FOUND
 
 # gcc -DHAS_JAVA_TREEVIEW -I$JNI_INCLUDE -I$JNI_INCLUDE_LINUX $LLVM_CFLAGS $LLVM_LDFLAGS -L$JNI_LIBRARY -g src/main.c -ljvm $LLVM_LIBS -o klc
 
